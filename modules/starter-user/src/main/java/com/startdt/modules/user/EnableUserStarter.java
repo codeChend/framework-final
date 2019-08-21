f016a92ad466dce3888a62f0bbe91759fac47c0e
zhangfengshi <zhangfengshi@ops.startdt.net>
2019-08-21 18:00:25 +0800

    11

--- modules/starter-user/src/main/java/com/startdt/modules/user/EnableUserStarter.java
+++ modules/starter-user/src/main/java/com/startdt/modules/user/EnableUserStarter.java
@@ -14,6 +14,7 @@ import java.lang.annotation.Target;
 //@Import(ContentImportSelector.class)
 //@Import(SimpleContentConfiguration.class)
 //@Import(CoreContentConfiguration.class)
+//@Import(UserStarterConfiguration.class)
 @Import(UserStarterConfiguration.class)
 public @interface EnableUserStarter {
 
