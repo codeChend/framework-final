1f6b8a11a60bf351bd49090f0cec98d45998adec
weilong <chendong@startdt.com>
2019-10-08 10:46:18 +0800

    修改swagger文档

--- modules/starter-role/src/main/java/com/startdt/modules/role/controller/GrantPermissionController.java
+++ modules/starter-role/src/main/java/com/startdt/modules/role/controller/GrantPermissionController.java
@@ -44,7 +44,7 @@ public class GrantPermissionController {
     }
 
     @GetMapping("/v1/functionPermission/{userId}")
-    @ApiOperation(value = "通过userId分层获取所有菜单权限")
+    @ApiOperation(value = "通过userId获取所有功能权限集")
     public Result<DataInfo<List<String>>> getFunctionPermission(@PathVariable("userId") Integer userId) {
 
         return Result.ofSuccess(DataInfo.resultToData(grantPermissionService.getFunctionPermission(userId)));
